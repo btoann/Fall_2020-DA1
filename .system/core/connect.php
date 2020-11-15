ce262cbdae08a377bf56eefa35fed555a058b8f2
btoann <nguyenbaotoan2001@gmail.com>
2020-11-16 02:32:33 +0700

    update MVC

--- .system/core/connect.php
+++ .system/core/connect.php
@@ -14,32 +14,4 @@
         return $DBH;
     }
 
-    function query($sql)
-    {
-        $conn = connect();
-        // Thực thi
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        // Kết quả trả về
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        return $stmt->fetchAll();
-    }
-
-    function queryOne($sql)
-    {
-        $conn = connect();
-        // Thực thi
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        // Kết quả trả về
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        return $stmt->fetch();
-    }
-
-    function execute($sql)
-    {
-        $conn = connect();
-        $conn->exec($sql);
-    }
-
 ?>
--- index.php
+++ index.php
@@ -7,8 +7,19 @@
         <link rel="stylesheet" href=".public/icons/css/fontello.css">
     </head>
     <body>
-        <i class="icon-duckduckgo"></i>
-        <i class="icon-appstore"></i>
-        <i class="icon-weibo-1"></i>
+        
+    <?php
+
+        $index_file = 'client/controller/home.php';
+        if(isset($_GET['ctrl']) && $_GET['ctrl'])
+        {
+            $filename = 'client/controller/'.$_GET['ctrl'].'.php';
+            include (file_exists($filename)) ? $filename : $index_file;
+        }
+        else
+            include $index_file;
+
+    ?>
+
     </body>
 </html>
