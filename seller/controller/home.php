3aea7b3281186ca23e15d8984d130861b7fa9a37
nguyenducsy <72013858+nguyenducsy@users.noreply.github.com>
2020-12-04 02:46:09 +0700

    update seller #12

--- seller/controller/home.php
+++ seller/controller/home.php
@@ -1,18 +1,25 @@
 <?php
 
-    if(!isset($_SESSION['sbs_seller_id']))
-    {
-        header('location: seller.php?ctrl=account&act=signin');
-    }
-    else
-    {
-        include 'seller/model/products.php';
-        $count_products = count_products($_SESSION['sbs_seller_id']);
-        
-        if(isset($_GET['act']) && $_GET['act'] == 'home')
-            include 'seller/view/home/index.php';
-        else
-            include 'seller/view/home/index.php';
-    }
+    // if(!isset($_SESSION['sbs_seller_id']))
+    // {
+    //     header('location: seller.php?ctrl=account&act=signin');
+    // }
+    // else
+    // {
+        // include 'seller/model/products.php';
+        // $count_products = count_products($_SESSION['sbs_seller_id']);
+        include '../view/home/head.php';
+
+    //     if(isset($_GET['act']) && $_GET['act'] == 'home')
+    //         include 'seller/view/home/head.php';
+    //     else
+    //         include 'seller/view/home/index.php';
+    // // }
+
+
+
+
+
+
 
 ?>
---
+++ seller/insert_admin.php
@@ -0,0 +1,27 @@
+<div class="center">
+                    <div class="center__content">
+                        <div class="center__title">Bảng điều khiển</div>
+                        <p class="center__desc">Chèn admin</p>
+                        <div class="center__formWrapper">
+                            <form action="" method="post">
+                                <div class="center__inputWrapper">
+                                    <label for="name">Tên admin</label>
+                                    <input type="text" placeholder="Tên admin">
+                                </div>
+                                <div class="center__inputWrapper">
+                                    <label for="email">Email</label>
+                                    <input type="text" placeholder="Email">
+                                </div>
+                                <div class="center__inputWrapper">
+                                    <label for="password">Mật khẩu</label>
+                                    <input type="text" placeholder="Mật khẩu">
+                                </div>
+                                <div class="center__inputWrapper">
+                                    <label for="image">Hình ảnh</label>
+                                    <input type="file">
+                                </div>
+                                <button class="btn" type="submit">Chèn</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
