e7f77a390b3f2aee26ad81b13427a9ab9942120e
btoann <nguyenbaotoan2001@gmail.com>
2020-12-18 05:17:55 +0700

    .

--- client/controller/home.php
+++ client/controller/home.php
@@ -1,9 +1,32 @@
 <?php
-
     
+    include_once '.system/lib/controller.php';
+
     if(isset($_GET['act']) && $_GET['act'] == 'home')
         include 'client/view/home/index.php';
     else
         include 'client/view/home/index.php';
+
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if(isset($_POST['search']) && $_POST['search'] == 'search')
+        {
+            include_once 'client/model/products.php';
+            $search = search($_POST['search']);
+            if(count($search) > 0)
+            {
+                $output = '';
+                foreach ($search as $row)
+                {
+                    $output .= '<p><a href=index.php?ctrl=products&act=detail&id='.$row['id'].'>'.$row['name'].'</a></p>';
+                }
+                echo $output;
+            }
+            else
+            {
+                echo '<p class="search_notfound">Không tìm thấy từ khoá</p>';
+            }
+        }
+    }
 
 ?>
--- client/model/products.php
+++ client/model/products.php
@@ -18,14 +18,8 @@
 
     function search($txt)
     {
-            /* Tìm kiếm sản phẩm theo tên + tác giả + nxb,
-                        tác giả theo tên  */
-        $sql = 'SELECT sp.id, sp.name, "sanpham" as source 
-                    FROM sanpham sp INNER JOIN nhaxuatban nxb ON sp.id_publisher = nxb.id 
-                                    INNER JOIN tacgia tg ON sp.id_author = tg.id 
-                WHERE sp.name LIKE "%'.$txt.'%" OR nxb.name LIKE "%'.$txt.'%" OR tg.name LIKE "%'.$txt.'%"';
-        $sql .= ' UNION SELECT id, name, "tacgia" as table_name FROM tacgia tg WHERE tg.name LIKE "%'.$txt.'%"';
-        $sql .= ' LIMIT 20';
+        $sql = 'SELECT pr.id, pr.name FROM products pr 
+                    WHERE pr.name LIKE "%'.$txt.'%" LIMIT 20';
         // Thực thi
         return query($sql);
     }
